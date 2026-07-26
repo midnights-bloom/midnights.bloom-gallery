aa475103865631a097159dfb9b02804c3754bf09
midnights-bloom <a.pana@hotmail.com>
2026-07-26 09:49:32 -0400

    Update USER, REPO, and ROOT constants

--- Gallery/script.js
+++ Gallery/script.js
@@ -1,6 +1,6 @@
-const USER="midnights.bloom-gallery";
-const REPO="midnights.bloom-gallery-template";
-const ROOT="assets";
+const USER="midnights-bloom";
+const REPO="midnights.bloom-gallery";
+const ROOT="Gallery";
  
 const gallery=document.getElementById("gallery");
 const back=document.getElementById("back");
